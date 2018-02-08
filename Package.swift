8a37dfe7ea31a94e9d6f6afe69cf722b7932e52d
Gleb Radchenko <rgs444@mail.ru>
2018-02-08 13:59:15 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,9 +4,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "CCommonCrypto",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ]
+    name: "CCommonCrypto"
 )
